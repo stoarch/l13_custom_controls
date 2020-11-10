a5800201b8122587699aa32c369a8fcf3e093910
afonin_v@itstep.academy <afonin_v@itstep.academy>
2020-11-10 21:11:20 +0600

    namespace for colorpicker corrected

--- L13WPFNetControls/ColorPicker.xaml.cs
+++ L13WPFNetControls/ColorPicker.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace l13_custom_controls
+namespace L13WPFNetControls 
 {
     /// <summary>
     /// Interaction logic for ColorPicker.xaml
